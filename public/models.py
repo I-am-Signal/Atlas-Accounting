39d17019af8c0641fb4e6316832c41419a65871e
Dylan Carder <wcarder1@students.kennesaw.edu>
2024-09-28 03:41:07 -0400

    Email and Suspension Additions (neither is complete)

--- public/models.py
+++ public/models.py
@@ -2,6 +2,7 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy import CheckConstraint
 from sqlalchemy.sql import func
+from datetime import datetime, timedelta
 
 class BaseColumnMixin:
     id = db.Column(db.Integer, primary_key=True)
@@ -39,6 +40,7 @@ class Credential(db.Model, BaseColumnMixin):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     password = db.Column(db.String(150))
     failedAttempts = db.Column(db.Integer, default=0)
+    expirationDate = db.Column(db.DateTime, default=lambda: datetime.now() + timedelta(days=365))
 
 class Company(db.Model, BaseColumnMixin):
     creator_of_company = db.Column(db.Integer, db.ForeignKey('user.id'))
