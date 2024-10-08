50e8fa993b44c785501de88e422148742ac38539
Garrett McGuire <72244805+GarrettMcGuire54@users.noreply.github.com>
2024-10-07 20:42:31 -0400

    we're back to normal

--- public/views.py
+++ public/views.py
@@ -24,12 +24,9 @@ views = Blueprint("views", __name__)
 @views.route("/", methods=["GET", "POST"])
 @login_required_with_password_expiration
 def home():
-    if 'administrator' == current_user.role:
-        view_users_link = f'<a href="{url_for('views.view_users')}"><button class="dashleft admin">View/Edit Users</button></a>'
-        view_coa_link = f'<a href="{url_for('chart.view_accounts')}"><button class="dashleft admin">View/Edit Accounts</button></a>'
-    
-    eventLogsLink = '#'
-    journalEntriesLink = '#'
+    if "administrator" == current_user.role:
+        view_users_link = f'<a href="{url_for("views.view_users")}"><button class="dashleft admin" data-toggle="tooltip" data-placement="right" title="Link to User List">View/Edit Users</button></a>'
+        view_coa_link = f'<a href="{url_for("chart.view_accounts")}"><button class="dashleft admin" data-toggle="tooltip" data-placement="right" title="Link to Chart of Accounts">View/Edit Accounts</button></a>'
 
     eventLogsLink = "#"
     journalEntriesLink = "#"
@@ -310,7 +307,6 @@ def pfp():
             BytesIO(image.file_data),
             mimetype=image.file_mime,
             as_attachment=False,
-
             download_name=image.file_name,
         )
 
@@ -324,4 +320,3 @@ def help():
         "user",
         render_template("help.html", user=current_user, homeRoute="/"),
     )
-
